a3a6616af570c57fce99db0125fc9d90ba53025f
Elisabeth1604 <mudrik.bs@yandex.ru>
2024-04-24 13:09:50 +0300

    tv down_platf move
    После падения телевизора платформа под ним поменяла цвет и черная платформа начала двигаться

--- Alone/Assets/Scripts/MovePlatform.cs
+++ Alone/Assets/Scripts/MovePlatform.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 
 public class MovePlatform : MonoBehaviour
 {
-    public Transform pos1, pos2;
+	public GameObject TV;
+	public GameObject platf;
+	private Animator anim;
+
+	public Transform pos1, pos2;
     public float speed = 1f;
     public Transform startPos;
 
     Vector3 nextPos;
 
 	void Start()
-	{
-		nextPos = startPos.position;
+	{		
+		nextPos = startPos.position;		
 	}
-	 void Update()
+	void Update()
+	{
+		anim = platf.GetComponent<Animator>();
+		if (anim.enabled == true)
+		{
+			Movement();
+		}
+	}
+	void Movement()
 	{
 		transform.position=Vector3.MoveTowards(transform.position, nextPos, speed*Time.deltaTime);
 		if(transform.position==pos1.position) 
