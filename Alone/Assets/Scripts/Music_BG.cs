4223a6c1529725d71f4b98103a1847709e50aec2
sofiyachizhkova <sofiyachizhkova@yandex.ru>
2024-04-25 23:43:52 +0300

    restart_2
    сделала файл, чтобы фоновая музыка не прекращалась после падения перса и после перехода на новый уровень и что-то еще, я уже не помню(

---
+++ Alone/Assets/Scripts/Music_BG.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_BG : MonoBehaviour
+{
+	public GameObject BGMusic;
+	private AudioSource AudioSource;
+	public GameObject[] objs11;
+
+	private void Awake()
+	{
+		objs11 = GameObject.FindGameObjectsWithTag("Sound");
+		if (objs11.Length == 0)
+		{
+			BGMusic = Instantiate(BGMusic);
+			BGMusic.name = "BGMusic";
+			DontDestroyOnLoad(BGMusic.gameObject);
+		}
+		else
+		{
+			BGMusic = GameObject.Find("BGMusic");
+		}
+	}
+
+	private void Start()
+	{
+		AudioSource = BGMusic.GetComponent<AudioSource>();
+	}
+}
