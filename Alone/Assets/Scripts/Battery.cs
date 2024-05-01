933e1555fc22d0d358dffad2f4986d236923f7bf
Elisabeth1604 <mudrik.bs@yandex.ru>
2024-05-01 14:23:57 +0300

    Fading+to 2 level
    переход на 2 уровень с затуханием

--- Alone/Assets/Scripts/Battery.cs
+++ Alone/Assets/Scripts/Battery.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using static System.Net.Mime.MediaTypeNames;
+using UnityEngine.SceneManagement;
+using UnityEditor.SearchService;
 
 public class Battery : MonoBehaviour
 {
 	public GameObject Player;
 	public GameObject battery;
 	public GameObject panel;
+	public GameObject FideIn;
 	private float distance;
 	public float interDist = 2f;
 	public KeyCode myKey = KeyCode.X;
 	private Animator anim;
 	public Sprite newSprite;
+
 	void Update()
 	{
 		distance = Vector3.Distance(Player.GetComponent<Transform>().position, transform.position);
@@ -24,9 +28,8 @@ public class Battery : MonoBehaviour
 				anim = GetComponent<Animator>();
 				anim.PlayInFixedTime("battery_move", 1, 0.0f);
 				battery.GetComponent<BoxCollider2D>().enabled = false;
-				//anim.Play("battery_get");
-				//panel.GetComponent<SpriteRenderer>().sprite = newSprite;
 				StartCoroutine(waiter());
+				
 			}
 		}
 	}
@@ -35,6 +38,8 @@ public class Battery : MonoBehaviour
 		anim.Play("battery_get");
 		yield return new WaitForSeconds((float)1.9);
 		panel.GetComponent<SpriteRenderer>().sprite = newSprite;
+		FideIn.GetComponent<Animator>	().enabled = true;
 	}
+	
 }
 		
