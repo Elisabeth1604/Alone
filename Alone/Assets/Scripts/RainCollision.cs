e890f19188f6934cddc63d0f26c8d20ca9c1804a
Elisabeth1604 <mudrik.bs@yandex.ru>
2024-05-05 00:21:38 +0300

    rain, battery, lifes
    умирает от 3х капелек, анимация батарейки

--- Alone/Assets/Scripts/RainCollision.cs
+++ Alone/Assets/Scripts/RainCollision.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Collision : MonoBehaviour
 {
-	
+	public int count = 0;
 	private void OnParticleCollision(GameObject other)
 	{
 		if (other.gameObject.tag=="Player")
 		{
-			
+			count++;
 			PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
-			if(playerHealth != null )
-				playerHealth.Die();
-			
+			if (count == 3)
+			{ 
+				SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+				count = 0;
+			}
+
+
 		}
 	}
 }
