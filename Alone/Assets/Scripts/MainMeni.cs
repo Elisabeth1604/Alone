75d7763da4df413eb6aca5a1c095664643ee1d71
sofiyachizhkova <sofiyachizhkova@yandex.ru>
2024-05-14 01:05:22 +0300

    opening2

--- Alone/Assets/Scripts/MainMeni.cs
+++ Alone/Assets/Scripts/MainMeni.cs
@@ -1,24 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
+using UnityEngine.Timeline;
 public class MainMeni : MonoBehaviour
 {
     public GameObject TimelineManager;
     public GameObject Start;
     public GameObject Quit;
+    public GameObject Enter;
+    private bool Timeline_is_active = false;
     public void PlayGame()
     {
 		TimelineManager.SetActive(true); //включили анимацию
-        Start.SetActive(false); //выключили кнопки
-		Quit.SetActive(false);
+        Timeline_is_active = true;
 		//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-		//Start.SetActive(true); //обратно вернули
-		//Quit.SetActive(true);
 	}
     public void QuitGame()
     {
         Debug.Log("Игра закрылась");
         Application.Quit();
     }
+
+	private void Update()
+	{
+        if (TimelineManager.GetComponent<PlayableDirector>().time >= 9.75f && TimelineManager.GetComponent<PlayableDirector>().time <= 9.76f && Timeline_is_active)
+		{
+
+			TimelineManager.GetComponent<PlayableDirector>().Pause();
+			Enter.GetComponent<Animator>().enabled = true;
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                Enter.SetActive(false);
+                TimelineManager.GetComponent<PlayableDirector>().Resume();
+            }
+		}
+	}
 }
