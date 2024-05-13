571652d7f2afff8195b12908ee89752643200bd8
Elisabeth1604 <mudrik.bs@yandex.ru>
2024-05-13 21:24:35 +0300

    opening1

--- Alone/Assets/Scripts/down_box.cs
+++ Alone/Assets/Scripts/down_box.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class down_box : MonoBehaviour
 {
     public GameObject player;
-    void F()
+	public GameObject box;
+	public GameObject fade;
+	public string sortingLayerName = "Foreground";
+
+	void F()
     {
-       player.GetComponent<Animator>().enabled = true;
+		player.GetComponent<Animator>().enabled = true;
+		box.GetComponent<Animator>().enabled = false;
+		player.GetComponent<Renderer>().sortingLayerName = sortingLayerName;
+		fade.GetComponent<Animator>().enabled = true;
+		fade.GetComponent<Animator>().Play("Fade_opening");
 	}
 }
