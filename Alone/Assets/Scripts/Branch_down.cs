6d1be3e4ed2360baa8d5828a530990c09eb7df1f
Aigul6 <ajgul_ganieva@list.ru>
2024-05-14 23:17:38 +0300

    sounds5

--- Alone/Assets/Scripts/Branch_down.cs
+++ Alone/Assets/Scripts/Branch_down.cs
@@ -6,6 +6,13 @@ using UnityEngine.SceneManagement;
 public class Branch_down : MonoBehaviour
 {
 	public GameObject Branch;
+	public AudioSource branchAudio;
+
+	void Start()
+	{
+		branchAudio = GetComponent<AudioSource>();
+	}
+
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.tag == "Player")
@@ -14,6 +21,11 @@ public class Branch_down : MonoBehaviour
 			Branch.GetComponent<Rigidbody2D>().mass = 20;
 			Branch.GetComponent<Rigidbody2D>().gravityScale = 2;
 			Branch.GetComponent<BoxCollider2D>().isTrigger = false;
+
+			if (branchAudio != null && !branchAudio.isPlaying)
+			{
+				branchAudio.PlayOneShot(branchAudio.clip);
+			}
 		}
 	}
 }
